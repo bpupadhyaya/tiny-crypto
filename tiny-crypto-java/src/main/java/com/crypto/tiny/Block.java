b6598692a9384d4ba477b3875cbbf87a789aec1a
bpupadhyaya <bpupadhyaya@gmail.com>
2022-10-30 15:13:33 -0700

    Adding difficulty, mining, and validating.

--- tiny-crypto-java/src/main/java/com/crypto/tiny/Block.java
+++ tiny-crypto-java/src/main/java/com/crypto/tiny/Block.java
@@ -4,11 +4,19 @@ package com.crypto.tiny;
  */
 import java.util.Date;
 
+/*
+    Difficulty, this is the number of 0’s they must solve for. Low difficulty like 1 or 2 can be solved nearly
+    instantly on most computers, suggest something around 4–6 for testing. As of Dec 2017,  Litecoin’s
+    difficulty was around 442,592.
+*/
+
+// Proof of Work
 public class Block {
     public String hash;
     public String previousHash;
     private String data;
     private long timeStamp; //as number of milliseconds since 1/1/1970
+    private int nonce;
 
     public Block(String data,String previousHash ) {
         this.data = data;
@@ -21,9 +29,19 @@ public class Block {
         String calculatedhash = HashUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty  "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 
 }
--- tiny-crypto-java/src/main/java/com/crypto/tiny/TinyBlockChain.java
+++ tiny-crypto-java/src/main/java/com/crypto/tiny/TinyBlockChain.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class TinyBlockChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 5;
     public static void main(String... args) {
         Block beginningBlock = new Block("The first block", "0");
         System.out.println("Hash for block 1 : " + beginningBlock.hash);
@@ -17,10 +18,40 @@ public class TinyBlockChain {
         System.out.println("Hash for block 3 : " + thirdBlock.hash);
 
         blockchain.add(new Block("The first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
         blockchain.add(new Block("The second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
         blockchain.add(new Block("The third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
     }
 }
